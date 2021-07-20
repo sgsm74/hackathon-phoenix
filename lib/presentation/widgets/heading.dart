8c604f898f99985d54856147aa6afce0f6094047
Saeed Ghasemi <s.gh4286@gmail.com>
2021-07-20 18:44:07 +0430

    adding see all to headings

--- lib/presentation/widgets/heading.dart
+++ lib/presentation/widgets/heading.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 
 Widget headingTitle(String title) {
-  return Padding(
-    padding: const EdgeInsets.only(left: 10.0),
-    child: Text(
-      title,
-      style: TextStyle(
-        fontSize: 20,
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Padding(
+        padding: const EdgeInsets.only(left: 10.0),
+        child: Text(
+          title,
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
       ),
-    ),
+      Padding(
+        padding: const EdgeInsets.only(right: 10.0),
+        child: Text(
+          "See all >",
+          style: TextStyle(
+            fontSize: 13,
+            color: Colors.blue[300],
+          ),
+        ),
+      ),
+    ],
   );
 }
